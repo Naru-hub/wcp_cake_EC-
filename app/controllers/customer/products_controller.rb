ff12c5bf5cc599f4431003c185df7f2f28f1e4d8
Naru-hub <1208ny0324@gmail.com>
2021-06-29 07:23:39 +0000

    add customer_products_controller

--- app/controllers/customer/products_controller.rb
+++ app/controllers/customer/products_controller.rb
@@ -1,7 +1,11 @@
 class Customer::ProductsController < ApplicationController
   def index
+    @products = Product.page(params[:page]).per(8)
+    @product_count = Product.count
   end
 
   def show
+    @product = Product.find(params[:id])
+    @cart_items = CartItems.new
   end
 end
