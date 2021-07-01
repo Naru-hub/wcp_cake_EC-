ec8e2cd0acea6a520a5608ef91e74f9ccf2c4d97
Naru-hub <1208ny0324@gmail.com>
2021-07-01 09:38:51 +0000

    cart_items_controller_index

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -1,4 +1,9 @@
 class Customer::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItems.where(customer_id: current_customer.id)
+    @total_price = 0
+    @cart_items.each do |cart_item|
+      @total_price += (cart_item.product.price * 1.1) * cart_item.amount
+    end
   end
 end
