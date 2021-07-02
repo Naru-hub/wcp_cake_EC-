e0b240cd5e650cc2fdee25b5584b22a37ed72159
Naru-hub <1208ny0324@gmail.com>
2021-07-02 15:51:17 +0000

    customer_customers_controller

--- app/controllers/customer/customers_controller.rb
+++ app/controllers/customer/customers_controller.rb
@@ -1,10 +1,13 @@
 class Customer::CustomersController < ApplicationController
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def confirm
+    @customer = current_customer
   end
 end
